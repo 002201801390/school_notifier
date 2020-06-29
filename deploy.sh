d2394d47b97f8242259b549bb52f1ffcbf12318f
Eduwardo Horibe <eduhoribe@gmail.com>
2020-06-29 13:48:01 -0300

    fixing deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ $1 stop &&
 mvn -f ./rest_api -DskipTests=true clean install &&
 mvn -f ./rest_api -DskipTests=true package &&
 rm -rf "$2"/school_notifier &&
-cp ./rest_api/target/SchoolNotifier-0.0.1-SNAPSHOT.war ~/Apache/Tomcat/apache-tomcat-9.0.33/webapps/ &&
+cp ./rest_api/target/SchoolNotifier-0.0.1-SNAPSHOT.war "$2" &&
 unzip -d "$2"/school_notifier "$2"/SchoolNotifier-0.0.1-SNAPSHOT.war &&
 rm -rf "$2"/SchoolNotifier-0.0.1-SNAPSHOT.war &&
 $1 run
